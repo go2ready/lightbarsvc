26ae31f60985cc41dd09dd6a4890485a4e48f68a
Richard Wang <shw@microsoft.com>
2019-08-14 21:29:45 +0100

    WIP url server

--- lightbar/views.py
+++ lightbar/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import View
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+
+from rest_framework.views import APIView
+
+# importing the requests library 
+import requests 
+import json
+import urllib
+
+# Front end app view that loads the app
+class FrontendAppView(View):
+  """
+  Serves the compiled frontend entry point (only works if you have run `yarn
+  run build`).
+  """
+  def get(self, request):
+    product_reference = request.GET.get('product_reference', '')
+
+    payload = {
+      'product_reference': product_reference,
+    }
+
+    try:
+      return render(request, 'lightbar/build/index.html', payload)
+    except FileNotFoundError:
+      return HttpResponse(
+        """
+        This URL is only used when you have built the production
+        version of the app. Visit http://localhost:3000/ instead, or
+        run `yarn run build` to test the production version.
+        """,
+        status=501,
+      )
