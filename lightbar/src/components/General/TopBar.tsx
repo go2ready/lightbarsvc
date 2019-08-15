56b92becd85b399f27db537577407b622a322a9d
Shiwei Wang <sw3812@ic.ac.uk>
2019-08-15 21:11:33 +0100

    Trying to find broken commit

--- lightbar/src/components/General/TopBar.tsx
+++ lightbar/src/components/General/TopBar.tsx
@@ -3,10 +3,13 @@ import * as React from 'react';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { withStyles, WithStyles, createStyles  } from '@material-ui/core/styles';
 
+import { animateScroll } from 'react-scroll';
+
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -16,6 +19,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 
 import IconButton from '@material-ui/core/IconButton';
 import VerticalAlignTopIcon from '@material-ui/icons/VerticalAlignTop';
+import HomeIcon from '@material-ui/icons/Home';
 
 function ElevationScroll(props : ElevationScroll) {
   const { children, window } = props;
@@ -74,6 +78,8 @@ export const TopBar = withStyles(styles)(
       const { classes } = this.props;
       const { showConfirmationBox } = this.state;
 
+      var self = this;
+
       return (
         <div className={classes.root}>
           <AppBar position="fixed" color="default">
@@ -83,7 +89,10 @@ export const TopBar = withStyles(styles)(
               </Typography>
  
               <span className={classes.toolbarButtons}>
-                <IconButton aria-label="to the top" color="inherit">
+                <IconButton aria-label="go back to homepage" color="inherit" onClick={self.OnLeave}>
+                  <HomeIcon />
+                </IconButton>
+                <IconButton aria-label="to the top" color="inherit" onClick={self.OnScrollToTop}>
                   <VerticalAlignTopIcon />
                 </IconButton>
               </span>
@@ -92,7 +101,7 @@ export const TopBar = withStyles(styles)(
           
           <Dialog
             open={showConfirmationBox}
-            onClose={this.OnImportHelpDismissed}
+            onClose={this.OnLeaveCancel}
             aria-labelledby="help-alert-dialog-title"
             aria-describedby="help-alert-dialog-description"
           >
@@ -108,13 +117,47 @@ export const TopBar = withStyles(styles)(
               </DialogContentText>
             </DialogContent>
             <DialogActions>
-              <Button onClick={this.OnImportHelpDismissed} color="secondary">
-                Okay
+              <Button onClick={self.OnNavigateAway} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={self.OnLeaveCancel} color="secondary">
+                Cancel
               </Button>
             </DialogActions>
           </Dialog>
         </div>
       );
+    }
+
+    private OnScrollToTop()
+    {
+      animateScroll.scrollToTop({
+        duration: 500,
+        delay: 10,
+        smooth: true,
+        offset: 0,
+      });
+    }
+    
+    private OnLeave()
+    {
+      this.setState({
+        ...this.state,
+        showConfirmationBox: true
+      });
+    }
+
+    private OnNavigateAway()
+    {
+
+    }
+
+    private OnLeaveCancel()
+    {
+      this.setState({
+        ...this.state,
+        showConfirmationBox: false
+      });
     }
   }
 )
