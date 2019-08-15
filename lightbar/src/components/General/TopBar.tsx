c3b2b205ac905ba7e404ccb7e6fe488cf4c5377e
Richard Wang <shw@microsoft.com>
2019-08-15 19:25:19 +0100

    Implementing dialogue

--- lightbar/src/components/General/TopBar.tsx
+++ lightbar/src/components/General/TopBar.tsx
@@ -8,6 +8,12 @@ import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 import IconButton from '@material-ui/core/IconButton';
 import VerticalAlignTopIcon from '@material-ui/icons/VerticalAlignTop';
 
@@ -50,14 +56,23 @@ const styles = (theme: Theme) => createStyles({
 export interface ITopBarProps extends WithStyles<typeof styles> {
 }
 
+export interface ITopBarState {
+  showConfirmationBox: boolean;
+}
+
 export const TopBar = withStyles(styles)(
-  class extends React.Component<ITopBarProps>{
+  class extends React.Component<ITopBarProps, ITopBarState>{
     constructor(props : ITopBarProps) {
       super(props);
+
+      this.state = {
+        showConfirmationBox: false,
+      };
     }
 
     public render() : JSX.Element {
       const { classes } = this.props;
+      const { showConfirmationBox } = this.state;
 
       return (
         <div className={classes.root}>
@@ -74,6 +89,30 @@ export const TopBar = withStyles(styles)(
               </span>
             </Toolbar>
           </AppBar>
+          
+          <Dialog
+            open={showConfirmationBox}
+            onClose={this.OnImportHelpDismissed}
+            aria-labelledby="help-alert-dialog-title"
+            aria-describedby="help-alert-dialog-description"
+          >
+            <DialogTitle id="help-alert-dialog-title">{"Shipping from outside the country"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="help-alert-dialog-description">
+                Your light with custom designed spectrum will be specially made for you in our factory, 
+                brefore deliverying to you via express shipping directly to your door 
+                FREE of charge (if you live in remote areas that UPS/USPS/DHL/FEDEX/DPD doesn't cover, please contact us first).
+                {<br/>}{<br/>}
+                However there might be custom duty or import tax which we will not be responsible for, should such charge occurs, they 
+                are normally collected by the shipping company or when you pick it up, please do not confuse them for additional shipping charge.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.OnImportHelpDismissed} color="secondary">
+                Okay
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       );
     }
