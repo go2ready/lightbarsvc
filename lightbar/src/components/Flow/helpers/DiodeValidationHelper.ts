2cc7b2bd23f43b3356cff0c130f4be26c250ff12
Richard Wang <shw@microsoft.com>
2019-08-20 22:03:38 +0100

    Diode validation code

--- lightbar/src/components/Flow/helpers/DiodeValidationHelper.ts
+++ lightbar/src/components/Flow/helpers/DiodeValidationHelper.ts
@@ -1,3 +1,7 @@
+import { PresetHelper } from './PresetHelper';
+
+import { LightBarStyle } from '../../../types/FlowState';
+
 export class DiodeValidationHelper
 {
   // return a sequence of invalid string
@@ -14,5 +18,58 @@ export class DiodeValidationHelper
     }
   
     return result
+  }
+
+  public static IsPreset(diodeSequence: string[]) : string | undefined 
+  {
+    var lightBarStyle = DiodeValidationHelper.GetLightbarStyleByDiodeSeq(diodeSequence);
+    if (lightBarStyle !== undefined)
+    {
+      var spectrumMap = DiodeValidationHelper.GetSpectrumMap(lightBarStyle);
+      spectrumMap.forEach((value: string[], key: string) => {
+        var mapValue = value.join('');
+        var diodeValue = diodeSequence.join('');
+        if (mapValue === diodeValue)
+        {
+          return key;
+        }
+      });
+    }
+
+    return undefined;
+  }
+
+  private static GetLightbarStyleByDiodeSeq(diodeSequence: string[]): LightBarStyle | undefined
+  {
+    var length = diodeSequence.length;
+    switch(length)
+    {
+      case 18:
+        return LightBarStyle.Sixty;
+      case 24:
+        return LightBarStyle.Ninety;
+      case 36:
+        return LightBarStyle.OneTwety;
+    }
+
+    return undefined;
+  }
+
+  private static GetSpectrumMap(lightBarStyle: LightBarStyle) {
+    var rightMap = new Map<string, string[]>();
+    switch(lightBarStyle)
+    {
+      case LightBarStyle.Sixty:
+        rightMap = PresetHelper.PresetMap600;
+        break;
+      case LightBarStyle.Ninety:
+        rightMap = PresetHelper.PresetMap900;
+        break;
+      case LightBarStyle.OneTwety:
+        rightMap = PresetHelper.PresetMap1200;
+        break;
+    }
+
+    return rightMap;
   }
 }
